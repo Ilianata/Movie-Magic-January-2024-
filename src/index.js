b99eaf96a077203f5d9b1edcfb0d06f8e80f702a
Iliana Slavcheva <124202194+Ilianata@users.noreply.github.com>
2024-01-18 23:57:30 +0200

    Extract express and handlebars configs

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 const express = require("express");
-const handlebars = require("express-handlebars");
-const path = require("path");
+
 const routes = require("./routes");
+const configHandlebars = require("./config/configHandlebars");
+const configExpres = require("./config/configExpress");
 
 const app = express();
 const port = 5000;
 
-app.engine(
-  "hbs",
-  handlebars.engine({
-    extname: "hbs",
-  })
-);
+configHandlebars(app);
+configExpres(app);
 
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "views"));
-
-app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));
