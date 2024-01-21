8526ab798ca886f47a779cef743310a7fd3125e2
Iliana Slavcheva <124202194+Ilianata@users.noreply.github.com>
2024-01-21 14:18:56 +0200

    Set dinamic details page

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -6,7 +6,7 @@ const movies = [
     director: "Spilberg",
     year: "2019",
     image: "/img/jungle-cruise.jpeg",
-    rating: "5",
+    rating: "2",
     description:
       "Description: The youngest of King Triton's daughters, Ariel is a beautiful and spirited young mermaid with a thirst for adventure. Longing to find out more about the world beyond the sea, Ariel visits the surface and falls for the dashing Prince Eric. Following her heart, she makes a deal with the evil sea witch, Ursula, to experience life on land.",
   },
