bb7b6507f01d217c5ffd156f0d5e93decd283236
Iliana Slavcheva <124202194+Ilianata@users.noreply.github.com>
2024-01-28 13:32:12 +0200

    Add mongoose

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const routes = require("./routes");
 const configHandlebars = require("./config/configHandlebars");
 const configExpres = require("./config/configExpress");
-const mongoose = require("mongoose");
 
 const app = express();
 const port = 5000;
@@ -14,7 +14,7 @@ configExpres(app);
 app.use(routes);
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/movies")
+  .connect("mongodb://127.0.0.1:27017/magic-movies")
   .then(() => console.log("DB Connected"))
   .catch((err) => console.log(err));
 
