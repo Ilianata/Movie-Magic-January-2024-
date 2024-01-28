df05d3417f53b249eb60ae0d4ea9eb1918ad7c79
Iliana Slavcheva <124202194+Ilianata@users.noreply.github.com>
2024-01-28 20:10:22 +0200

    Reset search page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -11,10 +11,11 @@ router.get("/about", (req, res) => {
   res.render("about");
 });
 
-router.get("/search", (req, res) => {
+router.get("/search", async (req, res) => {
   const { title, genre, year } = req.query;
-  const movies = movieService.search(title, genre, year);
-  res.render("search", { movies });
+  const movies = await movieService.search(title, genre, year).lean();
+
+  res.render("search", { movies, title, genre, year });
 });
 
 router.get("/404", (req, res) => {
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -6,24 +6,20 @@ exports.getAll = () => Movie.find();
 exports.getOne = (movieId) => Movie.findById(movieId).populate("casts");
 
 //TODO: filter reult in mongoDB
-exports.search = async (title, genre, year) => {
-  let result = await Movie.find().lean();
+exports.search = (title, genre, year) => {
+  let query = {};
 
   if (title) {
-    result = result.filter((movie) =>
-      movie.title.toLowerCase().includes(title.toLowerCase())
-    );
+    query.title = new RegExp(title, "i");
   }
   if (genre) {
-    result = result.filter(
-      (movie) => movie.genre.toLowerCase() === genre.toLowerCase()
-    );
+    query.genre = genre.toLowerCase();
   }
   if (year) {
-    result = result.filter((movie) => movie.year === year);
+    query.year = year;
   }
 
-  return result;
+  return Movie.find(query);
 };
 
 exports.create = (movieData) => Movie.create(movieData);
