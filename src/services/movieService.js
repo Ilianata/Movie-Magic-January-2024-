990dcc9ef78498b3ed46d2459c5275c2b9e7c2aa
Iliana Slavcheva <124202194+Ilianata@users.noreply.github.com>
2024-01-28 14:16:56 +0200

    Refactor movieService to use movie model

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,28 +1,12 @@
 const Movie = require("../models/Movie");
 
-const movies = [
-  {
-    _id: 1,
-    title: "Jungle Cuise",
-    genre: "Adventure",
-    director: "Spilberg",
-    year: "2019",
-    image: "/img/jungle-cruise.jpeg",
-    rating: "2",
-    description:
-      "Description: The youngest of King Triton's daughters, Ariel is a beautiful and spirited young mermaid with a thirst for adventure. Longing to find out more about the world beyond the sea, Ariel visits the surface and falls for the dashing Prince Eric. Following her heart, she makes a deal with the evil sea witch, Ursula, to experience life on land.",
-  },
-];
-
 exports.getAll = () => Movie.find();
 
-exports.getOne = (movieId) => {
-  const movie = movies.find((movie) => movie._id == movieId);
-  return movie;
-};
+exports.getOne = (movieId) => Movie.findById(movieId);
 
-exports.search = (title, genre, year) => {
-  let result = movies.slice();
+//TODO: filter reult in mongoDB
+exports.search = async (title, genre, year) => {
+  let result = await Movie.find().lean();
 
   if (title) {
     result = result.filter((movie) =>
