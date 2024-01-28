ba8283cbb1895df6df77d258028be2f52b10b517
Iliana Slavcheva <124202194+Ilianata@users.noreply.github.com>
2024-01-28 11:58:30 +0200

    Connect with DB

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const routes = require("./routes");
 const configHandlebars = require("./config/configHandlebars");
 const configExpres = require("./config/configExpress");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 5000;
@@ -11,5 +12,10 @@ configHandlebars(app);
 configExpres(app);
 
 app.use(routes);
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/movies")
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));
